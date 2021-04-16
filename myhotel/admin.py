61b7d1003121563d5132c4015a419120a55a68cc
Miro Zwering <mirozwering@gmail.com>
2021-04-16 08:29:23 +0200

    vrijdagochtend

--- myhotel/admin.py
+++ myhotel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ["id", "number", "category", "beds", "capacity"]
+    ordering = ["-id"]
+
+admin.site.register(Room, RoomAdmin)
